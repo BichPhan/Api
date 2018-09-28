2a7f0bb4ab61b7ab9a86643cebfe5e033e42db04
BichPhan <ngocbich.it96@gmail.com>
2018-09-28 09:19:54 +0700

    api

---
+++ Api2/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataModel;
+
+namespace Api2.Controllers
+{
+    [RoutePrefix("v1/Products/Product")]
+    public class ProductController : ApiController
+    {
+        DataAccess dt = new DataAccess();
+
+        // GET api/<controller>
+        // [GET("allok")]
+        [Route("all")]
+        public HttpResponseMessage Get()
+        {
+            var products = dt.getProduct();
+            if (products.Any())
+                return Request.CreateResponse(HttpStatusCode.OK, products);
+            return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Products not found");
+        }
+
+        // GET api/<controller>/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<controller>
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/<controller>/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/<controller>/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ DataModel/DataAccess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataModel;
+using System.Configuration;
+
+namespace DataModel
+{
+    public class DataAccess
+    {
+        WebApiDbEntities db = new WebApiDbEntities();
+
+        public List<Products> getProduct()
+        {
+            return db.Products.ToList();
+        }
+
+        public Tokens GenerateToken(int userId)
+        {
+            string token = Guid.NewGuid().ToString();
+            DateTime issuedOn = DateTime.Now;
+            DateTime expiredOn = DateTime.Now.AddSeconds(Convert.ToDouble("900"));
+            var tokendomain = new Tokens
+            {
+                UserId = userId,
+                AuthToken = token,
+                IssuedOn = issuedOn,
+                ExpiresOn = expiredOn
+            };
+            db.Tokens.Add(tokendomain);
+            db.SaveChanges();
+            return tokendomain;
+        }
+    }
+}
