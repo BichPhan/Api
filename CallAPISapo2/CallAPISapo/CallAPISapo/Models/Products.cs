b9259a8d67d31d38ff6b063889d02a65ba3ab3f5
PhanBich <ngocbich.it96@gmail.com>
2018-09-29 12:25:39 +0700

    2

---
+++ CallAPISapo2/CallAPISapo/CallAPISapo/Models/Products.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CallAPISapo.Models
+{
+    public class Products
+    {
+        public List<Product> products { get; set; }
+    }
+}
---
+++ CallAPISapo2/CallAPISapo/CallAPISapo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CallAPISapo.Startup))]
+namespace CallAPISapo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
